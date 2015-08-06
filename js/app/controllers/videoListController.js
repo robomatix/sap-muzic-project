e6f2f40e38146e15d79938de8605653097fca7df
robomatix <robomatix@robomatix.net>
2015-08-06 17:13:39 +0200

    Controller that display a list of video id

--- js/app/controllers/videoListController.js
+++ js/app/controllers/videoListController.js
@@ -8,17 +8,22 @@
     var controllers = angular.module('sapmuzicproject.controllers');
 
     controllers.controller('VideoListController', function ($rootScope, $scope) {
-        $scope.todoList = [];
+        $scope.videoListId = [90399590, 90413430, 90445673];// Probably will be in a config file later
+        $scope.videoList = [];
+        getVideos();
 
-        getTodos();
+        function getVideos() {
 
-        function getTodos() {
-            for (var i = 1; i < 11; i++) {
-                $scope.todoList.push({
+
+            for (var i = 0; i < $scope.videoListId.length; i++) {
+                $scope.videoList.push({
                     id: i,
-                    title: 'Todo ' + i
+                    videoId: $scope.videoListId[i]
                 });
+
             }
+
+
         };
     });
 })();
